2fe3c69324125d0ffc4ed8681e27b0e5337798e8
YigitAydinn <yigitaydin3737@gmail.com>
2024-09-20 23:42:55 +0300

    Update CyberStart_ Week2.py

--- CyberStart_ Week2.py
+++ CyberStart_ Week2.py
@@ -6,8 +6,8 @@ points = [(1, 2), (4, 6), (5, 8), (9, 2), (3, 7)]
 
 # 2. Adım: Öklid Mesafesi Hesaplayan Fonksiyon
 def euclideanDistance(point1, point2):
-    x1, y1 = point1  # İlk noktanın x ve y değerlerini alıyoruz
-    x2, y2 = point2  # İkinci noktanın x ve y değerlerini alıyoruz
+    x1, y1 = point1  
+    x2, y2 = point2  
     return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
 # 3. Adım: Tüm Mesafeleri Hesapla
